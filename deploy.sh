149e814870843e20d86ea208f7d0f48a33feca92
t4nello <sapon.wojciech@outlook.com>
2025-01-26 11:58:17 +0100

    adjusted limits, gitignore and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "MINECRAFT_SERVER_PORT=25565" > ./managament/managament-stack.env
+  PORT=25565
+else
+  PORT=$1
 fi
 
-if ! [[ "$1" =~ ^[0-9]+$ ]] || [ "$1" -lt 0 ] || [ "$1" -gt 65535 ]; then
-  echo "Error: Port number must be integer from range 0-65535"
+# Sprawdzenie, czy podany port jest liczbą z zakresu 0-65535
+if ! [[ "$PORT" =~ ^[0-9]+$ ]] || [ "$PORT" -lt 0 ] || [ "$PORT" -gt 65535 ]; then
+  echo "Error: Port number must be an integer from range 0-65535"
   exit 1
 fi
 
-echo "MINECRAFT_SERVER_PORT=$1" > ./managament/managament-stack.env
 
-echo "Port ustawiony na: $1"
+echo "MINECRAFT_SERVER_PORT=$PORT" > ./managament/managament-stack.env
+
+echo "Minecraft server port is set to: $PORT"
+
 
 export $(cat ./managament/managament-stack.env | xargs)
+
 
 processed="./managament/managament-stack-processed.yml"
 docker stack config --compose-file ./managament/managament-stack.yml > "$processed"
 
-#docker stack deploy --compose-file "$processed" managament-stack
 
+docker stack deploy --compose-file "$processed" managament-stack
